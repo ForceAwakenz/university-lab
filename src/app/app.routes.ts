086013bb914267fe056f31a448b1bb8c4d063618
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2024-12-15 20:48:31 +0200

    basic sceleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ComponentsComponent } from './pages/components/components.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
+import { LibrariesComponent } from './pages/libraries/libraries.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { TipsComponent } from './pages/tips/tips.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'components', component: ComponentsComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'tips', component: TipsComponent },
+  { path: 'libraries', component: LibrariesComponent },
+  { path: 'labs', component: LabsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
